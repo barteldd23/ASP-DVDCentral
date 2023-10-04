f09b94be38d9a1a90231f18b5a61e6def9d1df00
Dean Bartel <300089145@fvtc.edu>
2023-10-04 13:19:31 -0500

    Added UI and put Dependencies in the BL and BL.Models to reference the UI.

---
+++ DDB.DVDCentral.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DDB.DVDCentral.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
