245a4dcfd9d4bb26a98caa4a5e22472e7193791f
Dean Bartel <300089145@fvtc.edu>
2023-10-25 11:31:50 -0500

    Error with Component Sidebar

---
+++ DDB.DVDCentral.UI/ViewComponents/Sidebar.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DDB.DVDCentral.UI.ViewComponents
+{
+    public class Sidebar : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View(GenreManager.Load());
+        }
+    }
+}
