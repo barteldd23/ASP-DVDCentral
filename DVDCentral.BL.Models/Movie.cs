6572212c5cbc38058c58800236e0e7560204bbc0
Dean Bartel <300089145@fvtc.edu>
2023-11-25 09:52:13 -0600

    added Images to image Card

--- DVDCentral.BL.Models/Movie.cs
+++ DVDCentral.BL.Models/Movie.cs
@@ -17,6 +17,8 @@ namespace DVDCentral.BL.Models
         public required int FormatId { get; set; }
         public required int DirectorId { get; set; }
         public required int InStkQty { get; set; }
+
+        [DisplayName("Image")]
         public string? ImagePath { get; set; }
 
         public string RatingDescription { get; set; }
