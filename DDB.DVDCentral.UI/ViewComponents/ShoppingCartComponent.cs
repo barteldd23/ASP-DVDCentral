565e16a7eba03abfad5da9138bbc26a0eaccec96
Dean Bartel <300089145@fvtc.edu>
2023-12-02 15:44:39 -0600

    Add ShoppingCart Model and Component.

---
+++ DDB.DVDCentral.UI/ViewComponents/ShoppingCartComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DDB.DVDCentral.UI.ViewComponents
+{
+    public class ShoppingCartComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+    {
+        if (HttpContext.Session.GetObject<ShoppingCart>("cart") != null)
+        {
+            return View(HttpContext.Session.GetObject<ShoppingCart>("cart"));
+        }
+        else
+        {
+            return View(new ShoppingCart());
+        }
+   
+    }
+}
---
+++ DVDCentral.BL.Models/ShoppingCart.cs
@@ -0,0 +1,56 @@
+﻿using DVDCentral.BL.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDB.DVDCentral.BL.Models
+{
+    public class ShoppingCart
+    {
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+        public int NumItems { get; set; }
+        public int TotalItems
+        {
+            get
+            {
+                int count = 0;
+                foreach (OrderItem orderItem in OrderItems)
+                {
+                    count += orderItem.Quantity;
+                }
+
+                return count;
+            }
+        }
+
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [DisplayName("Sub Total")]
+        public double SubTotal { 
+            get 
+            {
+                double subTotal = 0;
+                foreach (OrderItem orderItem in OrderItems)
+                {
+                    subTotal += orderItem.TotalCost;
+                }
+
+                return subTotal;
+            } 
+        }
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [DisplayName("Tax")]
+        public double Tax { get { return SubTotal * .055; } }
+
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [DisplayName("Order Total")]
+        public double Total { get { return SubTotal + Tax; } }
+
+
+
+
+    }
+}
