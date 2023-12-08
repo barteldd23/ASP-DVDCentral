e2fe0768134b66b6ace157df218cc745ae7ae1a1
Dean Bartel <300089145@fvtc.edu>
2023-12-08 15:07:14 -0600

    Implemented API links on views

--- DDB.DVDCentral.UI/Controllers/RatingController.cs
+++ DDB.DVDCentral.UI/Controllers/RatingController.cs
@@ -2,11 +2,15 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using Microsoft.VisualStudio.Web.CodeGeneration.Design;
 
 namespace DDB.DVDCentral.UI.Controllers
 {
     public class RatingController : Controller
     {
+        #region "Pre Web-API"
         // GET: RatingController
         public ActionResult Index()
         {
@@ -111,5 +115,183 @@ namespace DDB.DVDCentral.UI.Controllers
                 return View(item);
             }
         }
+
+        #endregion
+
+
+        #region "Web-API"
+        private static HttpClient InitializeClient()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7143/api/");
+            return client;
+        }
+
+        public IActionResult Get()
+        {
+            ViewBag.Title = "Ratings";
+            HttpClient client = InitializeClient();
+
+            // Hit the API
+            HttpResponseMessage response = client.GetAsync("Rating").Result;
+
+            // Parse the result
+            string result = response.Content.ReadAsStringAsync().Result;
+            dynamic items = (JArray)JsonConvert.DeserializeObject(result);
+            List<Rating> itemsList = items.ToObject<List<Rating>>();
+
+            return View(nameof(Index), itemsList);
+        }
+
+        public IActionResult GetOne(int id)
+        {
+            ViewBag.Title = "Rating";
+            HttpClient client = InitializeClient();
+
+            // Hit the API
+            HttpResponseMessage response = client.GetAsync("Rating/" + id).Result;
+
+            // Parse the result
+            string result = response.Content.ReadAsStringAsync().Result;
+            dynamic item = JsonConvert.DeserializeObject(result);
+            Rating rating = item.ToObject<Rating>();
+
+            ViewBag.Subject = rating.Description;
+
+            return View(nameof(Details), rating);
+        }
+
+        public IActionResult Insert()
+        {
+            ViewBag.Title = "Create";
+            ViewBag.Subject = "Rating";
+            if (Authenticate.IsAuthenticated(HttpContext))
+                return View(nameof(Create));
+            else
+                return RedirectToAction("Login", "User", new { returnUrl = UriHelper.GetDisplayUrl(HttpContext.Request) });
+            
+
+            
+        }
+
+        [HttpPost]
+        public IActionResult Insert(Rating rating)
+        {
+            try
+            {
+                HttpClient client = InitializeClient();
+
+                //Convert our rating into Json to be sent to the api
+                string serializedObject = JsonConvert.SerializeObject(rating);
+                var content = new StringContent(serializedObject);
+                content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+
+                // Hit the API and send the content it needs to do the post
+                HttpResponseMessage response = client.PostAsync("Rating", content).Result;
+
+                return RedirectToAction(nameof(Get));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View(nameof(Create));
+            }
+        }
+
+        public IActionResult Update(int id)
+        {
+            ViewBag.Title = "Edit Rating";
+
+            HttpClient client = InitializeClient();
+
+            // Hit the API
+            HttpResponseMessage response = client.GetAsync("Rating/" + id).Result;
+
+            // Parse the result
+            string result = response.Content.ReadAsStringAsync().Result;
+            dynamic item = JsonConvert.DeserializeObject(result);
+            Rating rating = item.ToObject<Rating>();
+
+            ViewBag.Subject = rating.Description;
+
+            if (Authenticate.IsAuthenticated(HttpContext))
+                return View(nameof(Edit), rating);
+            else
+                return RedirectToAction("Login", "User", new { returnUrl = UriHelper.GetDisplayUrl(HttpContext.Request) });
+
+            
+        }
+
+        [HttpPost]
+        public IActionResult Update(int id, Rating rating)
+        {
+            try
+            {
+                HttpClient client = InitializeClient();
+
+                //Convert our rating into Json to be sent to the api
+                string serializedObject = JsonConvert.SerializeObject(rating);
+                var content = new StringContent(serializedObject);
+                content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+                // Hit the API and send the content it needs to do the PUT for edit/update
+                HttpResponseMessage response = client.PutAsync("Rating/" + id, content).Result;
+
+                return RedirectToAction(nameof(Get));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View(nameof(Edit));
+            }
+        }
+
+        public IActionResult Remove(int id)
+        {
+            ViewBag.Title = "Delete Rating";
+
+            HttpClient client = InitializeClient();
+
+            // Hit the API
+            HttpResponseMessage response = client.GetAsync("Rating/" + id).Result;
+
+            // Parse the result
+            string result = response.Content.ReadAsStringAsync().Result;
+            dynamic item = JsonConvert.DeserializeObject(result);
+            Rating rating = item.ToObject<Rating>();
+
+            ViewBag.Subject = rating.Description;
+
+            if (Authenticate.IsAuthenticated(HttpContext))
+                return View(nameof(Delete), rating);
+            else
+                return RedirectToAction("Login", "User", new { returnUrl = UriHelper.GetDisplayUrl(HttpContext.Request) });
+            
+        }
+
+        [HttpPost]
+        public IActionResult Remove(int id, Rating rating)
+        {
+            try
+            {
+                HttpClient client = InitializeClient();
+                // Hit the API
+                HttpResponseMessage response = client.DeleteAsync("Rating/" + id).Result;
+                return RedirectToAction(nameof(Get));
+
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View(nameof(Delete));
+            }
+        }
+
+
+
+
+        #endregion
+
     }
 }
